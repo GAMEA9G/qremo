7aeba50d5f44887003d89f4ff23ac5b293d780f7
GAMEA9G <janakjithnallat462@gmail.com>
2025-03-31 15:35:41 +0530

    added start practice menu

---
+++ lib/Practice.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+part of 'main.dart';
+
+class PracticePage extends StatelessWidget {
+  const PracticePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(),);
+  }
+}
